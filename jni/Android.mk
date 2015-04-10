5cb4e0068805916e6ba5d49b510e1e8e3e1df03c
Jan Peter Stotz <>
2015-04-10 11:34:25 +0200

    Generate position-independent (PIE) code (necessary for execution of inotifywait on Android 5)

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS   = -std=c99
+LOCAL_CFLAGS += -fPIE -std=c99
+LOCAL_LDFLAGS += -fPIE -pie
 
 LOCAL_C_INCLUDES := \
 	./jni/regex \
@@ -10,6 +11,7 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_MODULE    := inotifywait
 LOCAL_SRC_FILES := wrap.c common.c libinotifytools/inotifytools.c libinotifytools/redblack.c
+
 
 include $(BUILD_EXECUTABLE)
 
